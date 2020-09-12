7d550c18ab721b68a916bd30403ecc87fdca148a
vzapertistudio <67166773+vzapertistuqdio@users.noreply.github.com>
2020-09-12 15:04:12 +0300

    FirstCommit

---
+++ Tanks/Assets/MyScript/BotMovementController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BotMovementController : MonoBehaviour
+{
+    private NavMeshAgent navAgent;
+
+   [SerializeField] private Transform point1;
+    [SerializeField] private Transform point2;
+    private Transform currentPoint;
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        currentPoint = point1;
+    }
+
+    
+    private void Update()
+    {
+        
+        navAgent.SetDestination(currentPoint.position);
+    }
+    private void Swap()
+    {
+        if(currentPoint==point1)
+        {
+            currentPoint =point2;
+        }
+        else if(currentPoint==point2)
+        {
+            currentPoint = point1;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="point")
+        {
+            Swap();
+        }
+    }
+}
---
+++ Tanks/Assets/MyScript/TapController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TapController : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hitInfo;
+
+    private Vector3 clickPosition;
+
+    private NavMeshAgent navAgent;
+
+    [SerializeField] private LineRenderer lineRenderer;
+    private void Start()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+        lineRenderer.enabled = false;
+    }
+
+    
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+       
+        if (Input.GetMouseButton(0))
+        {
+           ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hitInfo))
+            {
+                lineRenderer.enabled = true;
+                
+                lineRenderer.SetPosition(1,hitInfo.point);
+                navAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
